91f0a17fe6a7b317a40d78cb56ea36bc881772b0
HeyAryan27 <aryan.sharma@oodles.io>
2024-09-27 14:46:22 +0530

    first commit

---
+++ src/components/Recurit.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "../_styles/index.scss";
+
+
+const Recurit = () => {
+  return (
+    <div className="p-4 shadow mb-4 bg-white">
+    
+    
+    </div>
+  )
+}
+
+export default Recurit;
